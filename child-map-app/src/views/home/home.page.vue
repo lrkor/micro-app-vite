3ee7adc584c4046f24dc4774a1ae52c1ac75d6a9
xuwenbin <xuwenbin@sfere-elec.com>
2022-08-16 16:17:48 +0800

    添加图表实例

--- child-map-app/src/views/home/home.page.vue
+++ child-map-app/src/views/home/home.page.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="home">home</div>
+    <div class="home">
+        <WaterLevel />
+        <Line />
+        <AreaStack />
+        <BarSimple />
+        <Map />
+    </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
-
+import Line from '@/components/Line.vue';
+import AreaStack from '@/components/AreaStack.vue';
+import BarSimple from '@/components/BarSimple.vue';
+import WaterLevel from '@/components/WaterLevel.vue';
+import Map from '@/components/Map.vue';
 export default defineComponent({
     name: 'Home',
-    components: {},
+    components: {Line, AreaStack, BarSimple, WaterLevel, Map},
     setup() {
         return {};
     },
